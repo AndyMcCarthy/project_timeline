16ecef3f698a466572b874e63f94d4f707ecc769
AndyMcCarthy <andrew.p.mccarthy@gmail.com>
2023-12-14 10:58:06 -0500

    server returning timeline data

--- server/server.py
+++ server/server.py
@@ -16,7 +16,8 @@ def return_home():
     { "id": "item2", "group":1, "content": "SNCA", "hyperlink": "https://patentimages.storage.googleapis.com/28/40/cd/b42ef8e3a84681/WO2023192977A2.pdf", "editable": "false", "type":'box', "className": "Patent", "start": '2023-10-05', "title": "siRNA SNCA"},
     { "id": "item3", "group":1, "content": "HTT", "hyperlink": "https://patentimages.storage.googleapis.com/25/2f/00/6e133bb7be71e1/WO2023076450A2.pdf", "editable": "false", "type":'box', "className": "Patent", "start": '2023-05-04', "title": "siRNA HTT"},
     { "id": "item4", "group":1, "content": "TTR", "hyperlink": "https://patentimages.storage.googleapis.com/28/b5/4b/1b0a9a9c0d0295/AU2023248138A1.pdf", "editable": "false", "type":'box', "className": "Patent", "start": '2023-10-12', "title": "siRNA TTR"},
-    { "id": "item5", "group":2, "content": 'Study 1', "editable": "false", "type":'range', "className": "External", "start": '2022-06-16', "end": '2022-06-29', "title": "hello alynylam"},
+    { "id": "item5", "group":1, "content": "ANGPTL7", "hyperlink": "https://patentimages.storage.googleapis.com/68/26/0a/c060ee00f0d055/WO2023056478A1.pdf", "editable": "false", "type":'box', "className": "Patent", "start": '2022-09-30', "title": "siRNA ANGPTL7"},  
+    { "id": "item6", "group":2, "content": 'Study 1', "editable": "false", "type":'range', "className": "External", "start": '2022-06-16', "end": '2022-06-29', "title": "hello alynylam"},
        
     ])
 
